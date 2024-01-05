634ce01478f9724b422115ceddcfce42bd11baa4
Tfreire455 <thiago1.000@outlook.com>
2024-01-05 19:40:47 -0300

    teste feito

--- apenasPares.js
+++ apenasPares.js
@@ -1,2 +1,15 @@
 //
-const nome = [];
+
+
+function verificarPares(quantidade) {
+
+    let numeros = [];
+    for (let i = 0; i < quantidade; i++) {
+        numeros[i] = i;
+        if (numeros[i] % 2 === 0) {
+            console.log(numeros[i])
+        };
+    };
+};
+
+verificarPares(100);
