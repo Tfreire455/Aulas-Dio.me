9de97a5480ed245db031e0969c4b94fe891a7158
Tfreire455 <thiago1.000@outlook.com>
2024-01-21 16:49:59 -0300

    ...

--- 4ExerciciosPromises.js
+++ 4ExerciciosPromises.js
@@ -3,17 +3,18 @@ const { error } = require("console")
 function verificarLetra(letra1, letra2) {
     return promessa = new Promise((resolve, rejects) => {
         setTimeout(() => {
-        if (letra1 > letra2) {
-            resolve(`Letra ${letra2} vem antes de ${letra1}`)
-        } else if (letra1 < letra2) {
-            rejects(`Letra ${letra2} vem depois de letra ${letra1}`)
-        } else if ((letra1 || letra2 === Number)) {
-            rejects('Digite apenas letras', error)
-        }}, 5000)
+            if (letra1 > letra2) {
+                resolve(`Letra ${letra2} vem antes de ${letra1}`)
+            } else if (letra1 < letra2) {
+                rejects(`Letra ${letra2} vem depois de letra ${letra1}`)
+            } else if ((letra1 || letra2 === Number)) {
+                rejects('Digite apenas letras', error)
+            }
+        }, 5000)
     })
 }
 
-verificarLetra(1, 1).then((mensagem) => {
+verificarLetra('a', 'z').then((mensagem) => {
     console.log(mensagem)
 }).catch((erro) => {
     console.error(erro)
