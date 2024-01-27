a2be44bdb2916b320f8ddd065157644b629f2fe2
Tfreire455 <thiago1.000@outlook.com>
2024-01-27 13:14:07 -0300

    ...

--- 5- portfolio-dio.me/data/skills.js
+++ 5- portfolio-dio.me/data/skills.js
@@ -1,6 +1,6 @@
 function updateHardskillsProfile(profileData) {
 
     const CSS3 = document.getElementById('profile.skills.hardskills');
-    CSS3.innerHTML = profileData.skills.hardSkills.map(skill => `<li class="css"><img id="skill-css-img" src="${skill.logo}" alt="${skill.name}"></li>`).join('')
+    CSS3.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}"></li>`).join(' ')
 
 }
