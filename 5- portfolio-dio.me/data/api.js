3c583f9d9d74aa845964d6eccaf12ab0d7d5939e
Tfreire455 <thiago1.000@outlook.com>
2024-01-27 13:03:04 -0300

    ...

--- 5- portfolio-dio.me/data/api.js
+++ 5- portfolio-dio.me/data/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json?token=GHSAT0AAAAAACMNLW2VN5XYL3Z2UJWCB4N4ZNVEQ7A'
+    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json?token=GHSAT0AAAAAACMNLW2VDFTNBHYD4EZGIVPUZNVE5YA'
     const fetching = await fetch(url)
     return await fetching.json()
 }
--- 5- portfolio-dio.me/data/skills.js
+++ 5- portfolio-dio.me/data/skills.js
@@ -1,13 +1,6 @@
-async function updateHardskillsProfile(profileData) {
+function updateHardskillsProfile(profileData) {
 
     const CSS3 = document.getElementById('profile.skills.hardskills');
-    CSS3.innerHTML = profileData.skills.hardSkills.map(skills => `<li class="css"><img id="skill-css-img" src="${skills.logo}" alt="${skills.name}"></li>`).join('')
+    CSS3.innerHTML = profileData.skills.hardSkills.map(skill => `<li class="css"><img id="skill-css-img" src="${skill.logo}" alt="${skill.name}"></li>`).join('')
 
 }
-
-(async () => {
-    const profileData = await fetchProfileData()
-    updateHardskillsProfile(profileData)
-})
-
-()
