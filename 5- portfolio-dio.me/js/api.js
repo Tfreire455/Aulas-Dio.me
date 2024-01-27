ee8751eea34d840149e85eefa7ed4da1dfbecfe3
Tfreire455 <thiago1.000@outlook.com>
2024-01-27 12:13:44 -0300

    Atualizando

--- 5- portfolio-dio.me/js/api.js
+++ 5- portfolio-dio.me/js/api.js
@@ -1,1 +1,6 @@
-const gitApi = {}
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json?token=GHSAT0AAAAAACMNLW2V3VMWLNJ7L23MJ44CZNVDTFA'
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
+
