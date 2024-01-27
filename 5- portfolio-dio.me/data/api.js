451ce4e97d24d624f7f7d28aa257ee0302e98870
Tfreire455 <thiago1.000@outlook.com>
2024-01-27 14:00:04 -0300

    ...

--- 5- portfolio-dio.me/data/api.js
+++ 5- portfolio-dio.me/data/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json?token=GHSAT0AAAAAACMNLW2UMEIC6ZCRSSTFD2JWZNVGOPQ'
+    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json?token=GHSAT0AAAAAACMNLW2UMI2EPZYNOGXZNNQIZNVGXSQ'
     const fetching = await fetch(url)
     return await fetching.json()
 }
